ce4870ad95a0f060980ad7230ec79b134cebaa20
Miguel Colom <miguel@mcolom.info>
2016-03-18 18:58:10 +0100

    CHistogram.cpp improved algorithm

--- framework/CHistogram.cpp
+++ framework/CHistogram.cpp
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <assert.h>
 #include "CHistogram.cpp"
 #include "operations.cpp"
 
@@ -40,10 +41,9 @@ public:
       \param *data Data that goes inside the bins
       \param *datal Data to used to compute the histogram limits
       \param N Number of elements in data and datal
-      \param adaptive True to force the same number of sample in each bin
     */
     CHistogram<T>(int bins, T *data, float *datal,
-               int N, bool adaptive);
+               int N);
     virtual ~CHistogram<T>();
     //
     //! Gets bin begining
@@ -94,111 +94,81 @@ private:
     float *num_elements;
     T **data_bins;
     float **datal_bins;
-    bool adaptive;
 };
 
 template <class T>
-CHistogram<T>::CHistogram(int bins, T *data, float *datal,
-                       int N, bool adaptive) {
-  this->adaptive = adaptive;
+CHistogram<T>::CHistogram(int bins, T *data, float *datal, int N) {
   this->bins = bins;
 
-  int samples_per_bin = (this->adaptive ? (int)floor(N / bins) : -1);
+  int samples_per_bin = (int)floor(N / bins);
+  if (N % bins != 0)
+    samples_per_bin++;
 
+  // Assign memory
   this->limits_begin = new float[bins];
   this->limits_end   = new float[bins];
 
   this->num_elements = new float[bins];
 
-  this->data_bins = new T*[this->bins];
-  this->datal_bins = new float*[this->bins];
+  this->data_bins = new T*[bins];
+  this->datal_bins = new float*[bins];
+
   // Initialize this->data_bins and this->datal_bins to NULL
   for (int i = 0; i < this->bins; i++) {
     this->data_bins[i] = NULL;
     this->datal_bins[i] = NULL;
   }
   
-  // Allocate N samples for each bin.
-  // If we need to save memory, allocate just N / (bins - 1) if adaptive.
-  bool save_memory = (N > 6000*6000);
-  int len_buffers;
-  if (save_memory && this->adaptive) {
-    if (bins > 1)
-      len_buffers = N / (bins - 1);
-    else
-      len_buffers = N;
-  }
-  else
-    len_buffers = N;
-
-  T *buffer = new T[len_buffers];
-  float *bufferl = new float[len_buffers];
+  // Allocate samples for each bin.
+  T *buffer = new T[N];
+  float *bufferl = new float[N];
   
   // Sort data by datal
   int *indices = new int[N];
   argsort(datal, indices, N);
-  
-  // Min and max
-  float min_datal = datal[indices[0]];
-  float max_datal = datal[indices[N-1]];
 
-  float step = (this->adaptive ? -1 : (max_datal - min_datal) / bins);
-
-  float lim0 = min_datal;
+  // Number of elements in the current bin  
   int elements_count = 0;
 
   int bin = 0;
-  for (int idx = 0; idx <= N; idx++) {    
-      float lim1 = 0;
-      bool finished_loading;
-      if (idx == N)
-        finished_loading = true;
-      else {
-        lim1 = datal[indices[idx]];
-        if (this->adaptive) {
-            finished_loading = !(bin == bins - 1) &&
-                                (elements_count >= samples_per_bin);
-        }
-        else
-            finished_loading = (lim1-lim0 >= step);    
-      }
+  for (int idx = 0; idx < N; idx++) {      
+      // Keep loading...
+      buffer[elements_count] = data[indices[idx]];
+      bufferl[elements_count] = datal[indices[idx]];
+      
+      elements_count++;
+      
+      if (idx == N-1 || elements_count == samples_per_bin) {
+          // Buffer for the bin full
 
-      //
-      if (finished_loading) { // Finished loading current bin
-          // Move data in the buffer to the bins
+          // Copy data to current bin
+          this->datal_bins[bin] = new float[elements_count];
+
           this->data_bins[bin] = new T[elements_count];
           memcpy(this->data_bins[bin],
                   buffer,
                   sizeof(T) * elements_count);
 
-          this->datal_bins[bin] = new float[elements_count];
           memcpy(this->datal_bins[bin],
                   bufferl,
                   sizeof(float) * elements_count);
 
-          // Update limits and number of elements of the bin
-          this->limits_begin[bin] = lim0;
-          this->limits_end[bin] = lim1;
-          this->num_elements[bin] = elements_count;          
+          // Write bin metadata
+          this->limits_begin[bin] = bufferl[0];
+          this->limits_end[bin] = bufferl[elements_count-1];
+          this->num_elements[bin] = elements_count;
 
-          //printf("B%d) (%.2f, %.2f), %d elements\n", bin, lim0, lim1, elements_count);
-          //
-          lim0 = lim1;
-          //
-          bin++;
+          /* printf("[%.3f, %.3f], %d\n",
+               this->limits_begin[bin], this->limits_end[bin],
+               elements_count); */
+
+          // Prepare for next bin
           elements_count = 0;
-      }
-      else { // Load...
-          buffer[elements_count] = data[indices[idx]];
-          bufferl[elements_count] = datal[indices[idx]];
-          elements_count++;
-      }
+          bin++;
+      }     
   }
-  this->limits_end[bins-1] = max_datal;
-  //this->limits_end[bins-1] = last_lim1 + last_bin_width; //lim1; //max_datal;
-  //printf("%.2f, %.2f, %.2f, %.2f\n", max_datal, lim0, lim1, this->limits_end[bins-1]);
-  this->bins = bins;
   
+  // Free memory
   delete[] indices;
   delete[] buffer;
   delete[] bufferl;
@@ -252,4 +222,3 @@ int CHistogram<T>::get_num_bins() {
 }
 
 #endif	/* CHISTOGRAM_H */
-
